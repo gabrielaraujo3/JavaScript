a21444ef8f73f487aed3a13e76934f1210ff27f8
gabrielaraujo3 <araujog388@gmail.com>
2022-03-23 12:59:51 -0300

    finalizado logger

--- status/status.js
+++ status/status.js
@@ -1,4 +1,5 @@
 const os = require('os')
+const log = require('./logger')
 
 setInterval(() => {
 
@@ -17,6 +18,9 @@ setInterval(() => {
   console.clear()
   console.log("====== PC STATS ======")
   console.table(stats)
+
+  log(`${JSON.stringify(stats)}\n`)
+
 }, 1000)
 
 
