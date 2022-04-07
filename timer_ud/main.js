83971d0ee7e8e9d59b65527c6bfdb07bc505dc68
gabrielaraujo3 <araujog388@gmail.com>
2022-04-07 04:21:28 -0300

    exercícios timer

---
+++ timer_ud/main.js
@@ -0,0 +1,47 @@
+function relogio() {
+  function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+      hour12: false,
+      timeZone: 'GMT'
+    });
+  }
+
+  const relogio = document.querySelector('.relogio');
+  const iniciar = document.querySelector('.iniciar');
+  const pausar = document.querySelector('.pausar');
+  const zerar = document.querySelector('.zerar');
+  let segundos = 0;
+  let timer;
+
+  function iniciaRelogio() {
+    timer = setInterval(function() {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+  }
+
+  document.addEventListener('click', function(e) {
+    const elemento = e.target;
+
+    if (elemento.classList.contains('zerar')) {
+      clearInterval(timer);
+      relogio.innerHTML = '00:00:00';
+      relogio.classList.remove('pausado');
+      segundos = 0;
+    }
+
+    if (elemento.classList.contains('iniciar')) {
+      relogio.classList.remove('pausado');
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+
+    if (elemento.classList.contains('pausar')) {
+      clearInterval(timer);
+      relogio.classList.add('pausado');
+    }
+  });
+}
+
+relogio();
