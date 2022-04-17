98afcd6c33ea58095177a2977355ff3bbaa766f6
gabrielaraujo3 <araujog388@gmail.com>
2022-04-17 11:21:07 -0300

    Separação escopo

--- codigos_livro/ex_22/main.js
+++ codigos_livro/ex_22/main.js
@@ -1,61 +1,61 @@
-let erros = [];
+function meuPrograma() {
+  let erros = [];
 
-const sorteado = Math.floor(Math.random() * 100) + 1;
+  const sorteado = Math.floor(Math.random() * 100) + 1;
+  const chances = 6
 
-const chances = 6
+  function apostarNumero() {
+    const inNumero = document.getElementById('inNumero');
+    const numero = Number(inNumero.value);
 
-function apostarNumero() {
-  const inNumero = document.getElementById('inNumero');
-  
-  const numero = Number(inNumero.value);
+    if (numero <= 0 || numero > 100 || isNaN(numero)) {
+      alert('Informe um número válido...');
+      inNumero.focus();
+      return;
+    }
 
-  if (numero <= 0 || numero > 100 || isNaN(numero)) {
-    alert('Informe um número válido...');
-    inNumero.focus();
-    return;
-  }
+    const outDica = document.getElementById('outDica');
+    const outErros = document.getElementById('outErros');
+    const outChances = document.getElementById('outChances');
 
-  const outDica = document.getElementById('outDica');
-  const outErros = document.getElementById('outErros');
-  const outChances = document.getElementById('outChances');
-
-  if (numero === sorteado) {
-    alert('Parabéns!! Você acertou!!!');
-    btApostar.disabled = true;
-    btJogar.className = 'exibe';
-    outDica.textContent = 'Parabéns!! Número sorteado: ' + sorteado;
-  } else {
-    if (erros.indexOf(numero) >= 0) {
-      alert('Você já apostou o número ' + numero + '. Tente outro...');
+    if (numero === sorteado) {
+      alert('Parabéns!! Você acertou!!!');
+      btApostar.disabled = true;
+      btJogar.className = 'exibe';
+      outDica.textContent = 'Parabéns!! Número sorteado: ' + sorteado;
     } else {
-      erros.push(numero);
-      let numErros = erros.length;
-      let numChances = chances - numErros;
-      outErros.textContent = numErros + ' (' + erros.join(', ') + ')';
-      outChances.textContent = numChances;
-
-      if (numChances === 0) {
-        alert('Suas chances acabaram...');
-        btApostar.disabled = true;
-        btJogar.className = 'exibe';
-        outDica.textContent = 'Game Over!! Número sorteado: ' + sorteado;
+      if (erros.indexOf(numero) >= 0) {
+        alert('Você já apostou o número ' + numero + '. Tente outro...');
       } else {
-        let dica = numero < sorteado ? 'maior' : 'menor';
-        outDica.textContent = 'Dica: Tente um número ' + dica + ' que ' + numero;
+        erros.push(numero);
+        let numErros = erros.length;
+        let numChances = chances - numErros;
+        outErros.textContent = numErros + ' (' + erros.join(', ') + ')';
+        outChances.textContent = numChances;
+
+        if (numChances === 0) {
+          alert('Suas chances acabaram...');
+          btApostar.disabled = true;
+          btJogar.className = 'exibe';
+          outDica.textContent = 'Game Over!! Número sorteado: ' + sorteado;
+        } else {
+          let dica = numero < sorteado ? 'maior' : 'menor';
+          outDica.textContent = 'Dica: Tente um número ' + dica + ' que ' + numero;
+        }
       }
     }
+    inNumero.value = '';
+    inNumero.focus();
   }
-  
-  inNumero.value = '';
-  inNumero.focus();
+
+  function jogarNovamente() {
+    location.reload();
+  }
+
+  const btApostar = document.getElementById('btApostar');
+  btApostar.addEventListener('click', apostarNumero);
+
+  const btJogar = document.getElementById('btJogar');
+  btJogar.addEventListener('click', jogarNovamente);
 }
-
-function jogarNovamente() {
-  location.reload();
-}
-
-const btApostar = document.getElementById('btApostar');
-btApostar.addEventListener('click', apostarNumero);
-
-const btJogar = document.getElementById('btJogar');
-btJogar.addEventListener('click', jogarNovamente);
+meuPrograma();
