d03a0d610edd7bd1b023d0f3278834665220d6fb
gabrielaraujo3 <araujog388@gmail.com>
2022-04-18 22:32:07 -0300

    Praticando

---
+++ codigos_livro/ex_30/main.js
@@ -0,0 +1,11 @@
+function montarDica() {
+  const inFruta = document.getElementById('inFruta');
+  const outDica = document.getElementById('outDica');
+
+  const fruta = inFruta.value;
+
+  if (fruta === '') {
+    alert('Informe a fruta...');
+    
+  }
+}
