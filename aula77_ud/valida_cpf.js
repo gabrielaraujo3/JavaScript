24312d7b0c01817a637f965bbdd6b3d1838d008c
gabrielaraujo3 <araujog388@gmail.com>
2022-04-20 23:32:56 -0300

    Exercício validação cpf base

---
+++ aula77_ud/valida_cpf.js
@@ -0,0 +1,25 @@
+function ValidaCPF(cpfEnviado) {
+  Object.defineProperty(this, 'cpfLimpo', {
+    get: function() {
+      return cpfEnviado.replace(/\D+/g, '');
+    }
+  });
+}
+
+ValidaCPF.prototype.valida = function() {
+  if (typeof this.cpfLimpo === 'undefined') return false;
+  if (this.cpfLimpo.length !== 11) return false;
+
+  const cpfParcial = this.cpfLimpo.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
+
+  return true;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+  const cpfArray = Array.from(cpfParcial);
+  console.log(cpfArray);
+};
+
+const cpf = new ValidaCPF('705.484.450-52');
+console.log(cpf.valida());
