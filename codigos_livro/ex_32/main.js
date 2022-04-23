454ffd162325765927cf7c43bed6fa29908f10f3
gabrielaraujo3 <araujog388@gmail.com>
2022-04-22 23:58:19 -0300

    Exercício substr

---
+++ codigos_livro/ex_32/main.js
@@ -0,0 +1,21 @@
+function gerarCracha() {
+  const inNome = document.getElementById('inNome');
+  const outCracha = document.getElementById('outCracha');
+
+  const nome = inNome.value;
+
+  if (nome === '' || nome.indexOf(' ') === -1) {
+    alert('Informe o nome completo do participante...');
+    inNome.focus();
+    return;
+  }
+
+  const priEspaco = nome.indexOf(' ');
+  const ultEspaco = nome.lastIndexOf(' ');
+
+  const cracha = nome.substr(0, priEspaco) + nome.substr(ultEspaco);
+
+  outCracha.textContent = 'Crachá: ' + cracha;
+}
+const btGerar = document.getElementById('btGerar');
+btGerar.addEventListener('click', gerarCracha);
