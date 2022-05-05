e0f5b0a203aeacc775ec87510fd65cabeadeb400
gabrielaraujo3 <araujog388@gmail.com>
2022-05-05 17:02:53 -0300

    Ajax then/catch

--- aula89_ud/assets/js/main.js
+++ aula89_ud/assets/js/main.js
@@ -1,14 +1,16 @@
 const request = obj => {
-  const xhr = new XMLHttpRequest();
-  xhr.open(obj.method, obj.url, true);
-  xhr.send();
-
-  xhr.addEventListener('load', () => {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      obj.success(xhr.responseText);
-    } else {
-      obj.error(xhr.statusText);
-    }
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
+  
+    xhr.addEventListener('load', () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    });
   });
 };
 
@@ -24,17 +26,15 @@ document.addEventListener('click', e => {
 
 function carregaPagina(el) {
   const href = el.getAttribute('href');
-
-  request({
+  
+  const objConfig = {
     method: 'GET',
     url: href,
-    success(response) {
-      carregaResultado(response);
-    },
-    error(errorText) {
-      console.log(errorText);
-    }
-  });
+  };
+
+  request(objConfig).then(response => {
+    carregaResultado(response);
+  }).catch(error => console.log(error));
 }
 
 function carregaResultado(response) {
