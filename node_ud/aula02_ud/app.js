ea64f77a626b88e801f28e269de60d99f1bf92b5
gabrielaraujo3 <araujog388@gmail.com>
2022-05-14 19:53:29 -0300

    Aula node 02 módulos

---
+++ node_ud/aula02_ud/app.js
@@ -0,0 +1,13 @@
+// const multiplicacao = require('./mod');
+// console.log(multiplicacao(2, 2));
+
+// const Cachorro = require('./B/C/D/mod');
+// const cachorrinho = new Cachorro('Dog');
+// cachorrinho.latir();
+
+// const Cachorro = require('./Z/mod2');
+// const c1 = new Cachorro('Dog');
+// c1.latir();
+
+const path = require('path');
+console.log(path.resolve(__dirname, '..', '..'));
