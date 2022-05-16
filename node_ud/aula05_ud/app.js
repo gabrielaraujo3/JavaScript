2340d9ac926da83dfe9daa0369aef67e06787604
gabrielaraujo3 <araujog388@gmail.com>
2022-05-16 13:32:06 -0300

    Aula node 05 FS write/read/json

---
+++ node_ud/aula05_ud/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, 'teste.json');
+const escreve = require('./module/escrever');
+const ler = require('./module/ler');
+
+const pessoas = [
+  { nome: 'João' },
+  { nome: 'Maria' },
+  { nome: 'Eduardo' },
+  { nome: 'Luiza' }
+];
+const json = JSON.stringify(pessoas, '', 2);
+escreve(caminhoArquivo, json);
+
+async function lerArquivo(caminho) {
+  const dados = await ler(caminho);
+  renderizaDados(dados); 
+}
+
+function renderizaDados(dados) {
+  dados = JSON.parse(dados);
+  dados.forEach(val => console.log(val.nome));
+}
+lerArquivo(caminhoArquivo);
