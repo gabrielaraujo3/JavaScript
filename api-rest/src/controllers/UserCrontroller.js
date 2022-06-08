4737e0d3a3f963da6bc920ada133a7b7aa805c06
Gabriel Araujo <araujog388@gmail.com>
2022-06-08 18:36:29 -0300

    Aula API Rest 04 CRUD de users parte 1

--- api-rest/src/controllers/UserCrontroller.js
+++ api-rest/src/controllers/UserCrontroller.js
@@ -4,11 +4,29 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
     }
   }
 }
--- api-rest/src/routes/userRoutes.js
+++ api-rest/src/routes/userRoutes.js
@@ -4,6 +4,8 @@ import userController from '../controllers/UserCrontroller';
 const router = new Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
 
 export default router;
 
