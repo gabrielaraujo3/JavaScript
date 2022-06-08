aefb6d4e9e38b6f0a096a6d5163cf642a61d7792
Gabriel Araujo <araujog388@gmail.com>
2022-06-08 19:07:53 -0300

    Aula API Rest 04 CRUD de users final

--- api-rest/src/controllers/UserCrontroller.js
+++ api-rest/src/controllers/UserCrontroller.js
@@ -29,6 +29,56 @@ class UserController {
       return res.json(null);
     }
   }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new UserController();
--- api-rest/src/routes/userRoutes.js
+++ api-rest/src/routes/userRoutes.js
@@ -6,6 +6,8 @@ const router = new Router();
 router.post('/', userController.store);
 router.get('/', userController.index);
 router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
 
